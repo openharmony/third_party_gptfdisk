50c1b8d71f52eb861ec6194c6961857b1c8c8c67
jiangqianrong <jiangqianrong1@huawei.com>
2026-06-26 13:28:15 +0800

    update: 更新文件 libpart.cpp
    Signed-off-by: jiangqianrong <jiangqianrong1@huawei.com>

---
+++ ohos/src/libpart/libpart.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include "libpart/libpart.h"
+
+int IoctlRetry(int fd) {
+   int ret = -1;
+   int retry_count = 0;
+   const int max_retries = 6;
+   const int retry_delay_us = 50000;
+
+   while (retry_count < max_retries) {
+      ret = ioctl(fd, BLKRRPART);
+      if (ret == 0) {
+         break;
+      }
+      retry_count++;
+      usleep(retry_delay_us * retry_count);
+   }
+
+   return ret;
+}
